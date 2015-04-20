df3ce7247eb940435f127d72533b19e23e6f881e
Angus Tasker <atasker2@gmail.com>
2015-04-19 22:12:47 -0400

    add comments

--- 7 kyu/musical_pitch_classes.rb
+++ 7 kyu/musical_pitch_classes.rb
@@ -14,3 +14,19 @@ def pitch_class(note)
     hash[note]
   end
 end
+
+# Description:
+
+# In music, each note is named by its pitch class
+# (e.g., C, E♭, F♯), and each pitch class can
+# alternatively be expressed as an integer from 0 to 11.
+# Your task will be to write a method called pitch_class
+# (JS: pitchClass ) that, when given a letter-based pitch
+# class, returns the corresponding integer.
+
+# Only seven letters are used to name the notes: "A" through "G."
+# These letter names are cyclical, just like the days of the week.
+# The notes corresponding to those letters are called the
+# "natural notes."
+
+
